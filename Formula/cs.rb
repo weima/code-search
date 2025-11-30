fd8824ba0cebee18de7e175b8c9e3f1b7495e691
Wei Ma <weima@pmfc.team>
2025-11-30 13:34:50 -0800

    chore: update homebrew formula to v0.1.2

--- Formula/cs.rb
+++ Formula/cs.rb
@@ -1,9 +1,9 @@
 class Cs < Formula
   desc "Intelligent code search tool for tracing text to implementation code"
   homepage "https://github.com/weima/code-search"
-  url "https://github.com/weima/code-search/releases/download/v0.1.0/cs-darwin-amd64"
-  sha256 "REPLACE_WITH_ACTUAL_SHA256"
-  version "0.1.0"
+  url "https://github.com/weima/code-search/releases/download/v0.1.2/cs-darwin-amd64"
+  sha256 "03950774584ed8b8439eac574e218ae3846323d2ce87bea09c8750ce5c6b7957"
+  version "0.1.2"
   license "Apache-2.0"
 
   def install
